3ea85838f3fc52044052511fd09c0658c5e1d41f
Oluwatosin Emmanuel Amokeodo <oluwatosin.amokeodo@gmail.com>
2023-04-26 13:59:14 +0100

    Update WorkerShiftInterface.php

--- app/Contracts/WorkerShiftInterface.php
+++ app/Contracts/WorkerShiftInterface.php
@@ -2,35 +2,4 @@
 
 namespace App\Contracts;
 
-interface WorkerShiftInterface
-{
-    /**
-     * @param $request
-     * @return mixed
-     */
-    public function workerClockIn($request): mixed;
-
-    /**
-     * @param $request
-     * @return mixed
-     */
-    public function workerClockOut($request): mixed;
-
-    /**
-     * @param $request
-     * @return mixed
-     */
-    public function listOfAllShiftForAWorker($request): mixed;
-
-    /**
-     * @param $request
-     * @return mixed
-     */
-    public function shiftManager($request): mixed;
-
-    /**
-     * @param $request
-     * @return mixed
-     */
-    public function dailyRoster($request): mixed;
-}
+interface WorkerShiftInterface{}
